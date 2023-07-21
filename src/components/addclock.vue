6d5cbc8d9fd32051f016d373d91d5069a38c3b4b
lVoFl <117173496+lVoFl@users.noreply.github.com>
2023-07-21 11:06:36 +0800

    Add files via upload

---
+++ src/components/addclock.vue
@@ -0,0 +1,47 @@
+<template>
+    <span><input v-model="name" placeholder="name"/></span>
+    <span><input v-model="time" placeholder="time" id="time"/>分钟</span>
+    <span>
+        <button @click="changetime(15)">15min</button>
+        <button @click="changetime(30)">30min</button>
+        <button @click="changetime(45)">45min</button>
+        <button @click="changetime(60)">1h</button>
+    </span>
+
+    <span>
+        <button @click="submit">确认</button>
+        <button @click="close">取消</button>
+    </span>
+</template>
+
+<script>
+// import { EventBus } from '../event-bus';
+export default {
+    data() {
+        return {
+            name: '',
+            time: '',
+            msg: '',
+        }
+    },
+    // mounted() {
+    //     EventBus.$on("aMsg", (msg) => {
+    //         this.msg = msg;
+    //     })
+    // },
+    methods: {
+        changetime(long) {
+            this.time = long;
+        },
+
+        close() {
+            this.$emit("change_showadd");
+        },
+
+        submit() {
+            this.close();
+        },
+        
+    }
+}
+</script>../event-bus
---
+++ src/components/sign up.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <input v-model="account"  placeholder="账号"/>
+      <input v-model="password"  placeholder="密码"/>
+      <button @click="submit">确认</button>
+      <button @click="close">取消</button>
+    </div>
+</template>
+  
+<script>
+  export default {
+    data() {
+      return {
+        account: '',
+        password: ''
+      };
+    },
+    methods: {
+      submit() {
+        
+        // const { account, password } = this;
+        // this.account = '';
+        // this.password = '';
+
+        this.close();
+      },
+
+      close() {
+        this.$emit("change_showup");
+      }
+    }
+  };
+</script>
+<style>
+    
+</style>
